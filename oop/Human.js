101131b22732bd969b5bbc45bef74d31e9b06d97
nsc-yut-fujita <63280648+nsc-yut-fujita@users.noreply.github.com>
2020-04-13 12:03:34 +0900

    クラスモデリング

---
+++ oop/Human.js
@@ -0,0 +1,6 @@
+class Yusya extends Status{
+    String name = null;
+    int hp = 0;
+    int MP = 0;
+    String buki = null;
+}
---
+++ oop/Status.js
@@ -0,0 +1,21 @@
+class Human{
+
+    String name = null;
+    int hp = 0;
+    int MP = 0;
+    String buki = null;
+
+    Human()
+    String getName(){
+        rturn name;
+    }
+    String getHp(){
+        rturn hp;
+    }
+    String getMp(){
+        rturn mp;
+    }
+    String getBuki(){
+        rturn buki;
+    }
+}
